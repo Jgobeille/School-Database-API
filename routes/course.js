4fc0ff53a844b19fde1b18967196c5e2c2d2db76
Jamie Gobeille <djent34@gmail.com>
2020-06-09 20:43:47 +0900

    set up basic routes and error validation in routes folders

--- routes/course.js
+++ routes/course.js
@@ -1,3 +1,104 @@
 const express = require('express');
 
+const { Course } = require('../models');
+
 const router = express.Router();
+
+function asyncHandler(cb) {
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next);
+    } catch (err) {
+      next(err);
+    }
+  };
+}
+
+// Send a GET request to /courses to READ a list of quotes
+router.get(
+  '/courses',
+  asyncHandler(async (req, res, _next) => {
+    const courses = await Course.findAll();
+
+    res.json(courses);
+  }),
+);
+
+// Send a GET request to /courses/:id - Returns a the course
+// (including the user that owns the course) for the provided course ID
+router.get(
+  '/courses/:id',
+  asyncHandler(async (req, res, next) => {
+    // get the id
+    const { id } = req.params;
+    // get the quote from the database
+    const course = await Course.findByPk(id);
+    // send the data to the browser as JSON
+    if (course) {
+      res.json(course);
+    } else {
+      res.status(400).json({ message: 'Course not found' });
+      next();
+    }
+  }),
+);
+
+// Send a POST request to /quotes to CREATE a new quote
+router.post(
+  '/courses',
+  asyncHandler(async (req, res, next) => {
+    if (req.body.title && req.body.description) {
+      const course = await Course.create({
+        title: req.body.title,
+        description: req.body.description,
+      });
+      res.status(201).json(course);
+    } else {
+      // change status to 400 if info missing
+      res.status(400).json({ message: 'title and description required' });
+      next();
+    }
+  }),
+);
+
+// Send a PUT request to /courses/:id UPDATE(edit) a course
+router.put(
+  '/courses/:id',
+  asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    // get course
+    const course = await Course.findByPk(id);
+    if (course) {
+      // reassign author and quote to new data user input
+      course.title = req.body.title;
+      course.description = req.body.description;
+
+      await Course.update(course);
+      // everything A O.K. status
+      // end method tells express server that route is completed
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: 'Course Not Found' });
+      next();
+    }
+  }),
+);
+
+// Send a DELETE request to /courses/:id DELETE a course
+router.delete(
+  '/courses/:id',
+  asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+
+    const course = await Course.findByPk(id);
+    if (course) {
+      await course.destroy();
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: 'Quote Not Found' });
+      next();
+    }
+  }),
+);
+
+module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,28 +1,80 @@
+// Express server
 const express = require('express');
 
+// imports
+
+const auth = require('basic-auth');
+const bcryptjs = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+
+// user model
+const { User } = require('../models');
+
+// router server
 const router = express.Router();
 
-// const { User } = require('../models');
+// validations for user
+const Validators = [
+  check('firstName')
+    .exists({
+      checkNull: true,
+      checkFalsy: true,
+    })
+    .withMessage('Please provide a value for "first name"'),
+  check('lastName')
+    .exists({
+      checkNull: true,
+      checkFalsy: true,
+    })
+    .withMessage('Please provide a value for "last name"'),
+  check('email')
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "Email"')
+    .isEmail()
+    .withMessage('Please provide a valid email address for "Email"'),
+  check('password')
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "Password"')
+    .isLength({ min: 8, max: 20 })
+    .withMessage('Please provide password with 8 to 20 characters'),
+];
 
 // Send GET request to /users to return currently authenticatedUser
-router.get('/users', (req, res) => {
+router.get('/users', authenticateUser, (req, res) => {
   const user = req.currentUser;
 
   res.json({
-    name: user.name,
-    username: user.username,
+    firstName: user.firstName,
+    lastName: user.lastName,
   });
 });
 
 // Send POST request to /users to create a new user
-router.post('/users', (req, res) => {
+router.post('/users', Validators, (req, res) => {
+  // Attempt to get the validation result from the Request object.
+  const errors = validationResult(req);
+
+  // If there are validation errors...
+  if (!errors.isEmpty()) {
+    const errorMessages = errors.array().map((error) => error.msg);
+
+    // Return the validation errors to the client.
+    return res.status(400).json({
+      errors: errorMessages,
+    });
+  }
+
   // Get the user from the request body.
   const user = req.body;
 
-  // add user to db
+  // Hash the new user's password
+  user.password = bcryptjs.hashSync(user.password);
+
+  // Add the user to the `users` array.
+  User.create(user);
 
   // Set the status to 201 Created and end the response.
-  return res.status(201).end();
+  res.status(201).end();
 });
 
 module.exports = router;
