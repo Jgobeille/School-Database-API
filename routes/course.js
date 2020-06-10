cd350c3d4e8ab69570c9560637346920e41cd60b
Jamie Gobeille <djent34@gmail.com>
2020-06-11 07:23:10 +0900

    refactored js functions to their own file. Now need to split up user and courses to their own files

--- routes/course.js
+++ routes/course.js
@@ -1,22 +1,13 @@
 const express = require('express');
 
-const auth = require('basic-auth');
 const bcryptjs = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
+
+const funcs = require('../js/functions.js');
 
 const { Course, User } = require('../models');
 
 const router = express.Router();
-
-function asyncHandler(cb) {
-  return async (req, res, next) => {
-    try {
-      await cb(req, res, next);
-    } catch (err) {
-      next(err);
-    }
-  };
-}
 
 // validations for user
 const validators = [
@@ -61,63 +52,14 @@ const validators = [
     .withMessage('Please provide password with 8 to 20 characters'),
 ];
 
-const authenticateUser = async (req, res, next) => {
-  let message = null;
-  // Parse the user's credentials from the Authorization header.
-
-  const credentials = auth(req);
-
-  if (credentials) {
-    const users = await User.findAll();
-
-    const user = users.find((u) => u.emailAddress === credentials.name);
-
-    // If a user was successfully retrieved from the data store...
-
-    if (user) {
-      // Use the bcryptjs npm package to compare the user's password
-      // (from the Authorization header) to the user's password
-      // that was retrieved from the data store.
-
-      const authenticated = bcryptjs.compareSync(
-        credentials.pass,
-        user.password,
-      );
-
-      // If the passwords match...
-      if (authenticated) {
-        req.currentUser = user;
-      } else {
-        message = `Authentication failure for username: ${user.firstName}`;
-      }
-    } else {
-      message = `User not found for username: ${credentials.firstName}`;
-    }
-  } else {
-    message = 'Auth header not found';
-  }
-
-  // If user authentication failed...
-  if (message) {
-    console.warn(message);
-    // Return a response with a 401 Unauthorized HTTP status code.
-    res.status(401).json({
-      message: 'Access Denied',
-    });
-  } else {
-    // Or if user authentication succeeded...
-    // Call the next() method.
-    next();
-  }
-};
-
 // Send GET request to /users to return currently authenticatedUser
-router.get('/users', authenticateUser, (req, res) => {
+router.get('/users', funcs.authenticateUser, (req, res) => {
   const user = req.currentUser;
 
   res.json({
     firstName: user.firstName,
     lastName: user.lastName,
+    emailAddress: user.emailAddress,
   });
 });
 
@@ -156,10 +98,21 @@ router.post('/users', validators, (req, res) => {
 // Send a GET request to /courses to READ a list of courses
 router.get(
   '/courses',
-  asyncHandler(async (req, res) => {
+  funcs.asyncHandler(async (req, res) => {
     const courses = await Course.findAll();
 
-    res.json(courses);
+    const filteredCourseList = courses.map((course) => {
+      return {
+        title: course.title,
+        description: course.description,
+        estimatedTime: course.estimatedTime,
+        materialsNeeded: course.materialsNeeded,
+        userId: course.userId,
+      };
+    });
+
+    // Need to loop over courses, filter the properties I need, then send through JSON
+    res.json({ filteredCourseList });
   }),
 );
 
@@ -167,14 +120,20 @@ router.get(
 // (including the user that owns the course) for the provided course ID
 router.get(
   '/courses/:id',
-  asyncHandler(async (req, res, next) => {
+  funcs.asyncHandler(async (req, res, next) => {
     // get the id
     const { id } = req.params;
     // get the quote from the database
     const course = await Course.findByPk(id);
     // send the data to the browser as JSON
     if (course) {
-      res.json(course);
+      res.json({
+        title: course.title,
+        description: course.description,
+        estimatedTime: course.estimatedTime,
+        materialsNeeded: course.materialsNeeded,
+        userId: course.userId,
+      });
     } else {
       res.status(400).json({ message: 'Course not found' });
       next();
@@ -185,13 +144,14 @@ router.get(
 // Send a POST request to /quotes to CREATE a new course
 router.post(
   '/courses',
-  authenticateUser,
-  asyncHandler(async (req, res, next) => {
+  funcs.authenticateUser,
+  funcs.asyncHandler(async (req, res, next) => {
     if (req.body.title && req.body.description) {
+      const currentUser = req.currentUser.id;
       const course = await Course.create({
         title: req.body.title,
         description: req.body.description,
-        userId: req.body.userId,
+        userId: currentUser,
       });
       res.status(201).json(course);
     } else {
@@ -205,16 +165,12 @@ router.post(
 // Send a PUT request to /courses/:id UPDATE(edit) a course
 router.put(
   '/courses/:id',
-  authenticateUser,
-  asyncHandler(async (req, res, next) => {
+  funcs.authenticateUser,
+  funcs.asyncHandler(async (req, res, next) => {
     const { id } = req.params;
-
-    console.log(req.currentUser.id);
 
     // get course
     const course = await Course.findByPk(id);
-
-    console.log(course.userId);
 
     if (course) {
       const currentUser = req.currentUser.id;
@@ -242,14 +198,21 @@ router.put(
 // Send a DELETE request to /courses/:id DELETE a course
 router.delete(
   '/courses/:id',
-  authenticateUser,
-  asyncHandler(async (req, res, next) => {
+  funcs.authenticateUser,
+  funcs.asyncHandler(async (req, res, next) => {
     const { id } = req.params;
 
     const course = await Course.findByPk(id);
     if (course) {
-      await course.destroy();
-      res.status(204).end();
+      const currentUser = req.currentUser.id;
+      if (course.userId === currentUser) {
+        await course.destroy();
+        res.status(204).end();
+      } else {
+        res.status(403).json({
+          message: 'This user is not authorized to delete this course',
+        });
+      }
     } else {
       res.status(404).json({ message: 'Course Not Found' });
       next();
